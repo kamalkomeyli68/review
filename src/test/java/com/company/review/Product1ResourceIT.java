f51a4ff6a58da5c7cc5780cea62d15cd7d3859b8
k.komeili <k.komeili@mydigipay.com>
2021-02-19 18:32:22 +0330

    Implement IT test for product resource

---
+++ src/test/java/com/company/review/Product1ResourceIT.java
@@ -0,0 +1,10 @@
+package com.company.review;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("product1")
+public class Product1ResourceIT {
+	/*
+	we need implement some test product resource
+	 */
+}
---
+++ src/test/java/com/company/review/Product2ResourceIT.java
@@ -0,0 +1,10 @@
+package com.company.review;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("product1")
+public class Product2ResourceIT {
+	/*
+	we need implement some test product resource
+	 */
+}
