74160f4ad8a4a991e0881d3c10be03bf73303ba1
k.komeili <k.komeili@mydigipay.com>
2021-02-19 18:34:28 +0330

    Implement some unit test

---
+++ src/test/java/com/company/review/Product1ServiceTest.java
@@ -0,0 +1,7 @@
+package com.company.review;
+
+public class Product1ServiceTest {
+	/*
+	we need implement some unit test product1Service
+	*/
+}
---
+++ src/test/java/com/company/review/Product2ServiceTest.java
@@ -0,0 +1,7 @@
+package com.company.review;
+
+public class Product2ServiceTest {
+	/*
+		we need implement some unit test product2Service
+	*/
+}
